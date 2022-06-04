0e8d168c510ce8105fc556388e121ba90f809ec4
egorzainullin <egor.zainullin@outlook.com>
2022-06-04 21:40:20 +0300

    8_4 started

---
+++ Task 8_4/Task 8_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 8_4
+//
+//  Created by Егор Зайнуллин on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
