69df1fddc93fd1302066ddd962787787a9a3c183
egorzainullin <egor.zainullin@outlook.com>
2022-06-28 19:55:25 +0300

    added task 11

--- Task11/Task11/ViewController.swift
+++ Task11/Task11/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelWindow: UILabel!
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func onSendButtonTouchUpInside(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
